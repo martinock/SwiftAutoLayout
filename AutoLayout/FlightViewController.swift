690a2b3d7008734253d6b8e819de08153a17cc04
Nakama <nakama@201546-iMac.local>
2018-10-31 08:46:19 +0700

    Initial Commit

---
+++ AutoLayout/FlightViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FlightViewController.swift
+//  AutoLayout
+//
+//  Created by Samuel Edwin on 09/08/18.
+//  Copyright © 2018 Samuel Edwin. All rights reserved.
+//
+
+import UIKit
+
+class FlightViewController: UIViewController {
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
